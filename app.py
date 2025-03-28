794b34eda726a6cf13fe2103d7178ad343305ce3
avasheedy <sheedy_ava@wheatoncollege.edu>
2025-03-28 13:54:41 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+
+#App Streamlit File
+
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load the trained Random Forest model
+rf_model = joblib.load('oysterworm.pkl')
+
+st.title('Polydora Intensity Predictor')
+
+temperature = st.number_input('Temperature (°C)', value=15.0)
+salinity = st.number_input('Salinity (ppt)', value=28.0)
+ph = st.number_input('pH', value=8.0)
+oyster_size = st.number_input('Oyster Size (mm)', value=80)
+
+if st.button('Predict Intensity'):
+    future_conditions = pd.DataFrame([[temperature, salinity, ph, oyster_size]],
+                                     columns=['Temperature (°C)', 'Salinity (ppt)', 'pH', 'Oyster Size (mm)'])
+    prediction = rf_model.predict(future_conditions)
+    st.write(f'Predicted Polydora Intensity: {prediction[0]:.2f}')
